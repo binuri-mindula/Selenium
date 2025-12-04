5874507d318dc4d73bc1b81aedfccff4e350ce62
Binuri Mindula <binuriwagarachchi@gmail.com>
2025-12-04 22:49:34 +0530

    Add winodws test

--- src/test/java/WindowTest.java
+++ src/test/java/WindowTest.java
@@ -5,17 +5,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class WindowTest {
     WebDriver driver;
 
     @BeforeMethod
-    public void OpenLinkPage(){
+    public void OpenLinkPage() throws InterruptedException {
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.leafground.com/window.xhtml");
+        Thread.sleep(3000);
 
     }
 
@@ -26,7 +29,7 @@ public class WindowTest {
 
         String oldWindow = driver.getWindowHandle();
         System.out.println("Parent winodw" + oldWindow);
-        WebElement openButton = driver.findElement(By.id("//*[@id='j_idt88:new']"));
+        WebElement openButton = driver.findElement(By.xpath("//*[@id='j_idt88:new']"));
         openButton.click();
         Thread.sleep(3000);
 
@@ -34,16 +37,59 @@ public class WindowTest {
         System.out.println("Handles size"+handles.size());
 
         //first method using foreach loop
-        for(String handle : handles){
-            System.out.println("Print new window handle"+handle);
-            driver.switchTo().window(handle);
+        for(String newWindow : handles){
+            System.out.println("Print new window handle"+ newWindow);
+            driver.switchTo().window(newWindow);
             System.out.println("Page title is  "+driver.getTitle());
         }
 
         driver.close();
+        driver.switchTo().window(oldWindow);
 
         WebElement optionbutton1 = driver.findElement(By.xpath("//*[@id='j_idt88:new']/span"));
        boolean openButtonResability = optionbutton1.isDisplayed();
         System.out.println("Button Visibility "+openButtonResability);
+
+
+        //second method - using list
+//        List <String> list = new ArrayList<String>(handles); //converting set to list
+//        if(list.size()>1){
+//            driver.switchTo().window(list.get(1));
+//            System.out.println("Child tab title is  "+driver.getTitle());
+//            driver.close();
+//            driver.switchTo().window(oldWindow);
+//        }
+//
+//        WebElement optionbutton2 = driver.findElement(By.xpath("//*[@id='j_idt88:new']/span"));
+//        boolean openButtonResability2 = optionbutton2.isDisplayed();
+//        System.out.println("Button Visibility "+openButtonResability2);
+
+
+        //find the number of open tabs
+        WebElement multiwindowButton = driver.findElement(By.xpath("//*[@id='j_idt88:j_idt91']"));
+        multiwindowButton.click();
+        Thread.sleep(3000);
+
+        Set<String> multiwindows = driver.getWindowHandles();
+        int windowsSize = multiwindows.size();
+        System.out.println("No of windows" + windowsSize);
+
+        //close all windows except primary
+        WebElement closeButton = driver.findElement(By.xpath("//*[@id='j_idt88:j_idt93']"));
+        closeButton.click();
+        Thread.sleep(3000);
+        Set<String> newWindowsHandles = driver.getWindowHandles();
+        for(String allWindows : newWindowsHandles){
+            if(!allWindows.equals(oldWindow)){
+                driver.switchTo().window(allWindows);
+                driver.close();
+
+            }
+        }
+        driver.quit();
+
+        //driver.close() -> close the single browser window driver which on focus
+        //driver.quit() -> close all browser windows
+
     }
 }
