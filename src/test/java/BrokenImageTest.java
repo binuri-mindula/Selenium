ee14cf35b699bec832ac85e377ecc641bdf1f469
Binuri Mindula <binuriwagarachchi@gmail.com>
2025-12-22 01:03:56 +0530

    Add broken image test'

--- src/test/java/BrokenImageTest.java
+++ src/test/java/BrokenImageTest.java
@@ -2,9 +2,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.util.List;
 
 public class BrokenImageTest {
@@ -12,13 +14,16 @@ public class BrokenImageTest {
 
     @BeforeMethod
     public void OpenLinkPage() {
-
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        File file = new File("C:\\Users\\Asus\\Downloads\\SelectorsHub-Chrome-Web-Store.crx");
+        options.addExtensions(file);
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://the-internet.herokuapp.com/broken_images");
+
     }
     @Test
     public void brokenTest() {
+        driver.get("https://the-internet.herokuapp.com/broken_images");
         List<WebElement> images =  driver.findElements(By.xpath("//div[@class='example']/img"));
         for(WebElement image :images){
             if(image.getAttribute("naturalWidth").equals("0")){
