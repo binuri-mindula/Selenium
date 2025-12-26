517e01b1055575725e1af50991d595f3764f0eb5
Binuri Mindula <binuriwagarachchi@gmail.com>
2025-12-26 21:24:23 +0530

    Update calendar element test

--- src/test/java/CalendarTest.java
+++ src/test/java/CalendarTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.List;
 
 public class CalendarTest {
     WebDriver driver;
@@ -20,16 +21,41 @@ public class CalendarTest {
     }
 
     @Test
-    public void calendarExample(){
+    public void calendarExample() {
         driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
 
         //method 1
-        WebElement datepickerElement = driver.findElement(By.xpath("//input[@id='datepicker']"));
-        datepickerElement.sendKeys("02/12/2025");
+//        WebElement datepickerElement = driver.findElement(By.xpath("//input[@id='datepicker']"));
+//        datepickerElement.sendKeys("02/12/2025");
+
 
         //method 2
-        WebElement datepickerElement2 =driver.findElement(By.xpath("//input[@id='datepicker']"));
-        driver.findElement(By.xpath("//span[@class='ui-datepicker-year']"));
+        WebElement datepickerElement = driver.findElement(By.xpath("//input[@id='datepicker']"));
+        datepickerElement.click();
+        selectFutureDate("2026", "October", "6");
+    }
+
+    public void selectFutureDate(String year, String month, String day) {
+
+        while(true) {
+            WebElement datepickerElement2 = driver.findElement(By.xpath("//input[@id='datepicker']"));
+            String actualYear = driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
+            String actualMonth = driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
+
+            if (actualYear.equals(year) && actualMonth.equals(month)) {
+                break;
+            }else{
+                driver.findElement(By.xpath("//a[@title='Next']")).click();
+            }
+        }
+
+        List<WebElement> alldates = driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']/tbody/tr//td//a"));
+        for (WebElement dateElement:alldates){
+            if (dateElement.getText().equals(day)){
+                dateElement.click();
+                break;
+            }
+        }
 
     }
 }
